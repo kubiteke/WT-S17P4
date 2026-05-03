e1d8942bf713fa40e67ee682751b813b07f0aa4a
kubiteke <kubilaytekee@gmail.com>
2026-05-03 15:01:26 +0300

    sqlproje1

--- src/main/java/com/workintech/sqlintro/repository/OgrenciRepository.java
+++ src/main/java/com/workintech/sqlintro/repository/OgrenciRepository.java
@@ -3,58 +3,136 @@ package com.workintech.sqlintro.repository;
 import com.workintech.sqlintro.entity.Ogrenci;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
 
     //Öğrenci tablosundaki tüm kayıtları listeleyin.
-    String QUESTION_1 = "";
+    String QUESTION_1 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci";
     @Query(value = QUESTION_1, nativeQuery = true)
     List<Ogrenci> findAll();
 
     //Öğrenci tablosundaki kız öğrencileri listeleyin.
-    String QUESTION_2 = "";
+    String QUESTION_2 = "select * from ogrenci\n" +
+            "where cinsiyet='K'";
     @Query(value = QUESTION_2, nativeQuery = true)
     List<Ogrenci> findGirls();
 
     //Öğrenci tablosunda kaydı bulunan sınıfların adını her sınıf bir kez görüntülenecek şekilde listeleyiniz
-    String QUESTION_3 = "";
+    String QUESTION_3 = "select \n" +
+            "\tdistinct sinif\n" +
+            "from ogrenci";
     @Query(value = QUESTION_3, nativeQuery = true)
     List<String> findAllClasses();
 
     //Öğrenci tablosunda, 10A sınıfında olan kız öğrencileri listeleyiniz.
-    String QUESTION_4 = "";
+    String QUESTION_4 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci\n" +
+            "where cinsiyet='K' and sinif='10A'";
     @Query(value = QUESTION_4, nativeQuery = true)
     List<Ogrenci> find10AGirls();
 
     //Öğrenci numarası 5 ile 10 arasında olan Kız öğrencileri listeleyiniz.
-    String QUESTION_5 = "";
+    String QUESTION_5 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci\n" +
+            "where cinsiyet='K' and (ogrno>5 and ogrno<10)";
     @Query(value = QUESTION_5, nativeQuery = true)
     List<Ogrenci> findGirlsWithOgrno();
 
     // Öğrencileri adına göre sıralayınız (alfabetik)
-    String QUESTION_6 = "";
+    String QUESTION_6 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci \n" +
+            "order by ad asc";
     @Query(value = QUESTION_6, nativeQuery = true)
     List<Ogrenci> findStudentsAlphabetically();
 
     // 10A sınıfındaki öğrencileri okul numarasına göre azalan olarak sıralayınız.
-    String QUESTION_7 = "";
+    String QUESTION_7 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci \n" +
+            "where sinif='10A'\n" +
+            "order by ogrno desc";
     @Query(value = QUESTION_7, nativeQuery = true)
     List<Ogrenci> find10AStudentsByOgrNo();
 
     //Öğrenciler tablosundaki en genç öğrenciyi listeleyiniz.
-    String QUESTION_8 = "";
+    String QUESTION_8 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci \n" +
+            "order by dtarih desc\n" +
+            "limit 1";
     @Query(value = QUESTION_8, nativeQuery = true)
     Ogrenci findYoungestStudent();
 
     //Öğrenciler tablosundaki en yaşlı öğrenciyi listeleyiniz.
-    String QUESTION_9 = "";
+    String QUESTION_9 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci \n" +
+            "order by dtarih asc\n" +
+            "limit 1";
     @Query(value = QUESTION_9, nativeQuery = true)
     Ogrenci findElderStudent();
 
     //İkinci harfi E olan ogrencileri listeleyiniz.
-    String QUESTION_10 = "";
+    String QUESTION_10 = "select \n" +
+            "\togrno,\n" +
+            "\tad,\n" +
+            "\tsoyad,\n" +
+            "\tcinsiyet,\n" +
+            "\tsinif,\n" +
+            "\tpuan,\n" +
+            "\tdtarih \n" +
+            "from ogrenci \n" +
+            "where ad ilike '_e%'";
     @Query(value = QUESTION_10, nativeQuery = true)
     List<Ogrenci> findStudentsSecondLetterOfN();
 
